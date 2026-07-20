559d249cc352788542aadcee91d12b8b2ad74eb5
ZT Winds <rodriguezedward6mbmq@gmail.com>
2026-07-20 21:04:14 +0800

    feat: add persisted club display ordering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'antd/dist/reset.css'
 import './styles.css'
 import './loading.css'
+import './admin-ordering.css'
 import App from './App'
 
 const client = new QueryClient({ defaultOptions: { queries: { staleTime: 15_000, retry: 1 } } })
